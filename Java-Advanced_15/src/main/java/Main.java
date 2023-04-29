da1857e56de1af92cc521f56b39b813dc41330ed
Otst0y <114588112+Otst0y@users.noreply.github.com>
2023-04-29 12:16:04 +0300

    Add files via upload

---
+++ Java-Advanced_15/src/main/java/Main.java
@@ -0,0 +1,68 @@
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class Main {
+    private static final SessionFactory ourSessionFactory;
+    private static final ServiceRegistry serviceRegistry;
+
+    static {
+        try {
+            Configuration configuration = new Configuration();
+            configuration.configure("hibernate.cfg.xml");
+
+            serviceRegistry = new ServiceRegistryBuilder().applySettings(configuration.getProperties()).buildServiceRegistry();
+            ourSessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        } catch (Throwable ex) {
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static Session getSession() throws HibernateException {
+        return ourSessionFactory.openSession();
+    }
+
+    public static void main(final String[] args) throws Exception {
+        final Session session = getSession();
+        Transaction transaction = session.beginTransaction();
+
+        PostEntity post = new PostEntity();
+        post.setTitle("Random Post");
+
+        CommentEntity comment_1 = new CommentEntity();
+        comment_1.setAuthorName("John");
+        comment_1.setPost(post);
+
+        CommentEntity comment_2 = new CommentEntity();
+        comment_2.setAuthorName("Doe");
+        comment_2.setPost(post);
+
+        CommentEntity comment_3 = new CommentEntity();
+        comment_3.setAuthorName("Homer");
+        comment_3.setPost(post);
+
+        CommentEntity comment_4 = new CommentEntity();
+        comment_4.setAuthorName("Lisa");
+        comment_4.setPost(post);
+
+        post.setComments(new HashSet<>(Arrays.asList(comment_1,comment_2,comment_3,comment_4)));
+
+        session.persist(post);
+        transaction.commit();
+
+        PostEntity postDB = (PostEntity) session.get(PostEntity.class, 11);
+        System.out.println(postDB + " --> " + postDB.getComments());
+
+        CommentEntity commentDB = (CommentEntity) session.get(CommentEntity.class,2);
+        System.out.println(commentDB + "---->" + commentDB.getPost());
+
+        session.close();
+    }
+}
